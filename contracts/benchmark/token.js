ee8e3c77d90dcaf7e2c85a11794c9a161fe19a60
Kirill Maksimov <kirill@orbs.com>
2019-10-30 11:12:59 +0200

    BenchmarkToken officially blessed by Tal

---
+++ contracts/benchmark/token.js
@@ -0,0 +1,43 @@
+import { Address, State, Verify, Uint64 } from "orbs-contract-sdk/v1";
+
+// Define uint64 later
+const TOTAL_SUPPLY = Uint64("10000000000");
+
+// doesn't do anything except validation
+export function TransferEvent(from, to, amount) {
+    Verify.bytes(from);
+    Verify.bytes(to);
+    Verify.uint64(amount);
+}
+
+export function _init() {
+    const ownerAddress = Address.getSignerAddress();
+    State.writeUint64(ownerAddress, TOTAL_SUPPLY)
+
+}
+
+export function transfer(amount, targetAddress) {
+    Verify.uint64(amount);
+    Verify.bytes(targetAddress);
+
+    // sender
+    const callerAddress = Address.getCallerAddress();
+    const callerBalance = State.readUint64(callerAddress);
+    if (callerBalance < amount) {
+        throw new Error(`transfer of ${amount} failed since balance is only ${callerBalance}`);
+    }
+    State.writeUint64(callerAddress, callerBalance-amount);
+
+    // recipient
+    Address.validateAddress(targetAddress);
+    const targetBalance = State.readUint64(targetAddress);
+    State.writeUint64(targetAddress, targetBalance+amount);
+
+    Events.emitEvent(TransferEvent, from, to, amount);
+}
+
+export function getBalance() {
+    Verify.Bytes(targetAddress);
+    Address.validateAddress(targetAddress);
+    return State.readUint64(targetAddress);
+}
