e82f3c369ff97c1b33412635da7a2d48899295d9
Kirill Maksimov <kirill@orbs.com>
2019-11-12 16:26:18 +0200

    Push to docker hub

---
+++ release/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+if [[ $CIRCLE_TAG == v* ]] ;
+then
+  VERSION=$CIRCLE_TAG
+else
+  VERSION=experimental
+fi
+
+VERSION="${VERSION}-js"
+
+docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD
+
+docker tag orbs:export orbsnetwork/node:$VERSION
+docker push orbsnetwork/node:$VERSION
+
+docker tag orbs:gamma-server orbsnetwork/gamma:$VERSION
+docker push orbsnetwork/gamma:$VERSION
